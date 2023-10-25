b8c8054d1e1836e5738e16d457857857a230d727
bmit-hug <lorenzo.hug@espas.ch>
2023-10-25 14:59:13 +0200

    sent from python script

--- git.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-commands = ["git add *", f'git commit -m "{input()}"', "git push"]
-
-for x in commands:
-    subprocess.run(x, shell=True)
---
+++ gitgit.py
@@ -0,0 +1,4 @@
+import subprocess
+
+for command in ["git add *", f'git commit -m "{input("Commit message: ")}"', "git push"]:
+    subprocess.run(command, shell=True)
