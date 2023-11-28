e37f207a41a1a027d7ee5cd01fa798a1206a3c4c
bmit-hug <lorenzo.hug@espas.ch>
2023-11-28 16:50:24 +0100

    update

--- scripts/js/navbar.js
+++ scripts/js/navbar.js
@@ -3,33 +3,29 @@ var aboutMe = document.getElementById("aboutMe");
 var mySkills = document.getElementById("mySkills");
 var contact = document.getElementById("contact");
 
+navbarList = [home, aboutMe, mySkills, contact];
+
 var item = document.getElementsByClassName("navbar-item");
 var stripeBox = document.getElementById("stripeBox");
 var toggle = false;
-
-home.style.display = "none";
-aboutMe.style.display = "none";
-mySkills.style.display = "none";
-contact.style.display = "none";
 
 function toggleNavbar() {
 
     // expand
     if (!toggle) {
 
-        setTimeout(function() {
+        setTimeout(()=> {
             home.classList.remove("home");
             aboutMe.classList.remove("aboutMe");
             mySkills.classList.remove("mySkills");
             contact.classList.remove("contact");
-        }, 10);
+        },10);
 
         stripeBox.style.transform = "rotate(90deg)";
-
-        home.style.display = "initial";
-        aboutMe.style.display = "initial";
-        mySkills.style.display = "initial";
-        contact.style.display = "initial";
+        
+        navbarList.forEach(element => {
+            element.style.display = "initial";
+        });
 
     // collapse
     } else {
@@ -41,13 +37,13 @@ function toggleNavbar() {
 
         stripeBox.style.transform = "rotate(0)";
 
-        setTimeout(function() {
-            home.style.display = "none";
-            aboutMe.style.display = "none";
-            mySkills.style.display = "none";
-            contact.style.display = "none";
+        setTimeout(()=> {
+            navbarList.forEach(element => {
+                element.style.display = "none";
+            });
         }, 400);
     }
+
     toggle = !toggle;
     console.log("navbar expanded:", toggle);
 }
