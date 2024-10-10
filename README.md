b7269a9cf9741690de07d723024f6c53bd0a8c8b
Lorenzo Hug <lorenzo.hug@espas.ch>
2024-10-10 15:42:09 +0200

    test

--- README.md
+++
@@ -1,1 +0,0 @@
-# [My Website](https://hug.bm-it.ch)
--- js/background.js
+++
@@ -1,7 +0,0 @@
-var backgroundGif = document.getElementById("backgroundGif")
-var speed = 0.1
-
-window.addEventListener("scroll", () => {
-    var scrollPosition = window.scrollY
-    backgroundGif.style.transform = "translateY(-" + scrollPosition * speed + "px)"
-})
